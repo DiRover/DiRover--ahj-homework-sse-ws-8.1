cca72e1ae8889d2e32ede8bb43afca7d024b97f9
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-12-27 19:22:36 +0300

    code v2.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-console.log('ok');
 import API from './api';
+
+console.log('ok');
 
 export const container = document.querySelector('[data-id="container"]');
 const url = 'http://localhost:7871/sse';
@@ -8,8 +9,8 @@ const api = new API(url);
 const eventSource = api.getEventSource();
 eventSource.addEventListener('message', api.load);
 eventSource.addEventListener('open', (evt) => {
-    console.log('connected');
+  console.log('connected');
 });
 eventSource.addEventListener('error', (evt) => {
-    console.log('error');
+  console.log('error');
 });
