5fb09ff3978d5d0fe577265bc5bcedfcb79e84bc
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-02-02 00:03:18 +0300

    code v2.1

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import API from './api';
 console.log('ok');
 
 export const container = document.querySelector('[data-id="container"]');
+
 // const url = 'http://localhost:7871/sse';
 const url = 'https://sse-8-1-server.herokuapp.com/sse';
 const api = new API(url);
